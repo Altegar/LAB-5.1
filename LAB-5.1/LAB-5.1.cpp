1b411656f4cc128c196381b19f0812a7c4a57ab5
Sushomeister <91095463+Sushomeister@users.noreply.github.com>
2021-11-03 16:50:39 +0200

    Добавьте файлы проекта.

---
+++ LAB-5.1/LAB-5.1.cpp
@@ -0,0 +1,32 @@
+﻿// LAB-5.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// Lab_05_1.cpp
+// Сушинський Ігор
+// Лабораторна робота № 5.1
+// Функції, що містять арифметичний вираз.
+// Варіант 18
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double g(const double x, const double y); // прототип
+
+int main()
+{
+	double r, s;
+
+	cout << "r = "; cin >> r;
+	cout << "s = "; cin >> s;
+
+	double c = (pow(g(r, s * s + 1), 2) * g * (r, s * s + 1) + g * (1, r * r + s)) / (1 + pow(g(1, r * s), 2) * g * (1, r * s));
+
+	cout << "c = " << c << endl;
+
+	return 0;
+}
+
+double g(const double x, const double y) // визначення
+{
+	return ((sin(x) * sin(x) + cos(y) * cos(y)) / (1 + abs(x * y)));
+}
